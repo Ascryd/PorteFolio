2bab4a92a7aa54773b04684f10ec2e42f9f7db9e
Cyril Florval <ascread@gmail.com>
2022-04-20 15:59:04 +0200

    About basique créé

--- src/components/AboutSection.vue
+++ src/components/AboutSection.vue
@@ -8,7 +8,15 @@
 
         <div class="content">
             <div class="informations">
-                <p>Bonjour !</p>
+                <p> 
+                    Bonjour, je m’appelle Cyril et j’ai découvert le développement web début 2021, après avoir travaillé dans l’immobilier, l’éducation ou encore à l’usine, j’ai enfin trouvé ma voie dans la création de site web, c’est un métier qui demande de la créativité, de l’organisation et surtout de la débrouillardise, bref, tout ce que j’aime !
+                </p>
+                <p> 
+                    J’ai donc commencé à apprendre le HTML/CSS avant de commencer une formation intense de Développeur Web chez OpenClassrooms, ou j’ai dû réaliser des projets de plus en plus complets pour apprendre et améliorer mes compétences.
+                </p>
+                <p> 
+                    Aujourd’hui, j’ai terminé cette formation et je réalise des projets personnels pour m’améliorer toujours plus dans ce domaine en attendant de pouvoir intégrer une entreprise pour me professionnaliser et continuer cette aventure.
+                </p>
 
             </div>
 
@@ -31,7 +39,7 @@ export default {
 <style scoped lang="scss">
 
 .about {
-    padding-top: 150px;
+    padding-top: 200px;
     display: flex;
     flex-direction: column;
     gap: 30px;
@@ -43,40 +51,44 @@ export default {
         align-items: center;
         gap: 20px;
 
-
         h2 {
             min-width: 155px;
             font-size: 30px;
-            
         }
 
         .line {
             height: 2px;
             width: 60%;
             background-color: rgb(255, 255, 255);
-            
         }
     }
 
     .content {
         display: flex;
+        justify-content: center;
+        gap: 30px;
         margin: auto;
-        width: 60%;
-        // background-color: rgb(117, 0, 0);
-        margin-left: 19%;
-        display: flex;
-        justify-content: space-around;
+        padding: 0 30px;
+        @include about-wrap;
         
 
         .informations {
-            width: 500px;
-            background-color: rgb(160, 0, 0);
+            min-width: 250px;
+            max-width: 750px;
+            // background-color: rgb(160, 0, 0);
+            display: flex;
+            flex-direction: column;
+            gap: 25px;
+
+            p {
+                font-size: 17px;
+                color: rgb(216, 216, 216);
+            }
         }
 
         .photo {
             margin: 0 10px 0 10px;
         }
-        
     }
 }
     
